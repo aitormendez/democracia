07f2044b8964c76783eb7547d9e839e4d2b039f4
aitor <aitor@e451.net>
2020-04-01 17:04:58 +0200

    Hamburger icon

--- web/app/themes/dc/resources/views/partials/header.blade.php
+++ web/app/themes/dc/resources/views/partials/header.blade.php
@@ -2,10 +2,18 @@
   <div class="logo bg-n d-flex align-items-center justify-content-center p-5">
     @svg('logo')
   </div>
-  <a class="brand mb-3" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-  <nav class="nav-primary">
-    @if (has_nav_menu('primary_navigation'))
-      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav flex-column align-items-start']) !!}
-    @endif
-  </nav>
+  <div class="solapa">
+    <a class="brand mb-3" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
+    <nav class="nav-primary">
+      @if (has_nav_menu('primary_navigation'))
+        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav flex-column align-items-start']) !!}
+      @endif
+    </nav>
+  </div>
+  <button class="hamburger hamburger--arrow" type="button">
+    <span class="hamburger-box">
+      <span class="hamburger-inner"></span>
+    </span>
+  </button>
+
 </header>
