fd646841f6ceee2f0c40e3d38e01c3bee1f25d11
aitor <aitor@e451.net>
2020-04-01 13:47:43 +0200

    Extended CPTs plugin

---
+++ web/app/mu-plugins/dc/dc-CPT.php
@@ -0,0 +1,142 @@
+<?php
+/**
+ * Plugin Name: dc-CPT
+ * Description: CPT para dc
+ * Author: Aitor Méndez
+ * Author URI: https://e451.net
+ * Texto Domain: dc-CPT
+ * License: MIT License
+ * https://github.com/johnbillion/extended-cpts
+ */
+
+namespace dc;
+
+add_action( 'init', function() {
+
+  // load_textodomain('dc-CPT', WPMU_PLUGIN_DIR . '/' .plugin_basename( dirname( __FILE__ ) ) . '/languages/dc-CPT-' . get_locale() . '.mo');
+
+
+  // Noticia cpt (story)
+  // --------------------------------------------------------------------------------
+
+  $labels_story = [
+    'name'                  => _x( 'Noticias', 'Post Type General Name', 'dc-CPT' ),
+  	'singular_name'         => _x( 'Noticia', 'Post Type Singular Name', 'dc-CPT' ),
+  	'menu_name'             => __( 'Noticias', 'dc-CPT' ),
+  	'name_admin_bar'        => __( 'Noticia', 'dc-CPT' ),
+  	'archives'              => __( 'Archivo de noticias', 'dc-CPT' ),
+  ];
+
+  $cols_story = [
+    'post_author' => [
+      'title'      => 'Post author',
+      'post_field' => 'post_author',
+    ],
+  ];
+
+  $filters_story = [
+
+  ];
+
+  $supports_story = [
+    'author',
+    'title',
+    'editor',
+    'excerpt',
+    'thumbnail',
+  ];
+
+  register_extended_post_type( 'story',
+    [
+      'show_in_rest' => true,
+      'show_in_feed' => true,
+      'labels'       => $labels_story,
+      'admin_cols'   => $cols_story,
+      'admin_filters'=> $filters_story,
+      'supports'     => $supports_story,
+    ],
+    [
+      'slug' => 'stories'
+    ]
+  );
+
+  // CV cpt (cv)
+  // --------------------------------------------------------------------------------
+
+  register_extended_post_type( 'cv',
+    [
+      'show_in_rest' => true,
+      'show_in_feed' => true,
+      // 'labels'       => $labels_story,
+      // 'admin_cols'   => $cols_story,
+      // 'admin_filters'=> $filters_story,
+      // 'supports'     => $supports_story,
+    ],
+    [
+      'singular' => 'CV',
+      'plural'   => 'CV items',
+      'slug'     => 'cv'
+    ]
+  );
+
+  // Proyectos cpt (project)
+  // --------------------------------------------------------------------------------
+
+  $labels_project = [
+    'name'                  => _x( 'Proyectos', 'Post Type General Name', 'dc-CPT' ),
+  	'singular_name'         => _x( 'Proyecto', 'Post Type Singular Name', 'dc-CPT' ),
+  	'menu_name'             => __( 'Proyectos', 'dc-CPT' ),
+  	'name_admin_bar'        => __( 'Proyecto', 'dc-CPT' ),
+  	'archives'              => __( 'Archivo de proyectos', 'dc-CPT' ),
+  ];
+
+  register_extended_post_type( 'project',
+    [
+      'show_in_rest' => true,
+      'show_in_feed' => true,
+      'labels'       => $labels_project,
+      // 'admin_cols'   => $cols_story,
+      // 'admin_filters'=> $filters_story,
+      // 'supports'     => $supports_story,
+    ]
+  );
+
+  // Crítica online cpt (critic)
+  // --------------------------------------------------------------------------------
+
+  $labels_crítica = [
+    'name'                  => _x( 'Críticas', 'Post Type General Name', 'dc-CPT' ),
+  	'singular_name'         => _x( 'Crítica', 'Post Type Singular Name', 'dc-CPT' ),
+  	'menu_name'             => __( 'Críticas', 'dc-CPT' ),
+  	'name_admin_bar'        => __( 'Crítica', 'dc-CPT' ),
+  	'archives'              => __( 'Archivo de críticas', 'dc-CPT' ),
+  ];
+
+  register_extended_post_type( 'critic',
+    [
+      'show_in_rest' => true,
+      'show_in_feed' => true,
+      'labels'       => $labels_crítica,
+      // 'admin_cols'   => $cols_story,
+      // 'admin_filters'=> $filters_story,
+      // 'supports'     => $supports_story,
+    ]
+  );
+
+  // TV cpt (tv)
+  // --------------------------------------------------------------------------------
+
+  register_extended_post_type( 'tv',
+    [
+      'show_in_rest' => true,
+      'show_in_feed' => true,
+      // 'labels'       => $labels_crítica,
+      // 'admin_cols'   => $cols_story,
+      // 'admin_filters'=> $filters_story,
+      // 'supports'     => $supports_story,
+    ]
+  );
+
+
+
+}, 0 );
