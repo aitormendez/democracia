09bc65be19cb4911d9de3ff8f547a02e441476d5
aitor <aitor@e451.net>
2020-04-28 10:57:20 +0200

    Bug desplegable proyectos en page-load infinite scroll. Sin resolver

--- web/app/themes/dc/resources/assets/scripts/routes/archive-project.js
+++ web/app/themes/dc/resources/assets/scripts/routes/archive-project.js
@@ -3,19 +3,6 @@ export default {
     // JavaScript to be fired on the about us page
 
 
-    // pintar de blanco el header del article desplegado
 
-    let desplegar = $('.btn-desplegar');
-
-    desplegar.click(function() {
-      desplegar.parents('article').removeClass('desplegado');
-      if ($(this).hasClass('collapsed')) {
-        $(this).parents('article').addClass('desplegado');
-        console.log('si');
-      } else {
-        $(this).parents('article').removeClass('desplegado');
-        console.log('no');
-      }
-    });
   },
 };
--- web/app/themes/dc/resources/assets/scripts/routes/archive.js
+++ web/app/themes/dc/resources/assets/scripts/routes/archive.js
@@ -3,10 +3,24 @@ var InfiniteScroll = require('infinite-scroll');
 export default {
   init() {
 
+      // pintar de blanco el header del article desplegado
+
+      let desplegar = $('.btn-desplegar');
+
+      desplegar.click(function() {
+        desplegar.parents('article').removeClass('desplegado');
+        if ($(this).hasClass('collapsed')) {
+          $(this).parents('article').addClass('desplegado');
+        } else {
+          $(this).parents('article').removeClass('desplegado');
+        }
+      });
+
     // infinite-scroll
     // -----------------------------------------------
 
     let buttonCont = $('.button-container');
+
 
     let main = new InfiniteScroll( '.infinite-scroll-container', {
       path: '.nav-previous a',
@@ -21,8 +35,15 @@ export default {
     console.log('lc: ' + main.loadCount);
 
     function onPageLoad() {
-      console.log(main.loadCount);
-      console.log('main.loadCount');
+      desplegar = $('.btn-desplegar');
+      desplegar.click(function() {
+        desplegar.parents('article').removeClass('desplegado');
+        if ($(this).hasClass('collapsed')) {
+          $(this).parents('article').addClass('desplegado');
+        } else {
+          $(this).parents('article').removeClass('desplegado');
+        }
+      });
       if ( main.loadCount == 1 ) {
         main.options.loadOnScroll = false;
         buttonCont.removeClass('d-none');
